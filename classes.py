ba8144c4733a69a0006d1ece1492d1ee58070c91
Psyborg0ne <artinopoulos.orfeas@gmail.com>
2020-09-18 12:30:40 +0300

    Classes, sp Attacks and Items
    ADD Class Blacksmith
    ADD Class Scavenger
    ADD spAttack placeholder for every class
    ADD Items file
    ADD Color yellow in colors

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
 from codeblin_utils import printc
+from time import sleep
+import colors
+
 
 class NPC():
     def __init__(self, name: str, desc: str, stats: dict, hpMult: int, mpMult: int):
@@ -32,6 +35,10 @@ class Paladin(NPC):
         self.mpMult = 2
         super().__init__(self.name, self.desc, self.stats, self.hpMult, self.mpMult)
 
+    def spDivineLight(self):
+        # TODO Heal the character and maybe add 1 turn of invinsibility (Holy Shield)
+        printc(f"{self.name} raises his hand and calls forth a divine beam of light {colors.YELLOW}healing {colors.RESET}him")
+
 class Assasin(NPC):
     def __init__(self, name):
         self.name = name
@@ -41,6 +48,11 @@ class Assasin(NPC):
         self.mpMult = 2
         super().__init__(self.name, self.desc, self.stats, self.hpMult, self.mpMult)
 
+    def spShadowStep(self, enemy: NPC):
+        # Double weapon DMG and chance to instakill or stun
+        printc(f"{self.name} hides and emerges from the shadows behind {enemy.name} striking a weak spot.")
+
+
 class Shaman(NPC):
     def __init__(self, name):
         self.name = name
@@ -49,3 +61,35 @@ class Shaman(NPC):
         self.hpMult = 10
         self.mpMult = 3
         super().__init__(self.name, self.desc, self.stats, self.hpMult, self.mpMult)
+
+    def spGaiaFlower(self, enemy: NPC):
+        # Roots the target enemy with chance to inflict poison?
+        printc(f"{self.name} touches the ground calling the spirits of the forest")
+        sleep(1)
+        printc(f"Thorny vines emerge from below and {colors.BOLD}root {colors.RESET}{enemy.name} in place")
+
+class Blacksmith(NPC):
+    def __init__(self, name):
+        self.name = name
+        self.desc = "A very strong character that dual wield hammers to deal massive amounts of damage."
+        self.stats = {'str': 18, 'agi': 2, 'int': 0}
+        self.hpMult = 7
+        self.mpMult = 0
+        super().__init__(self.name, self.desc, self.stats, self.hpMult, self.mpMult)
+
+    def spInferno(self, enemy: NPC):
+        # TODO Add Ablaze effect
+        printc(f"{self.name} starts hitting the ground violently with his hammers causing a magma eruption below {enemy.name}")
+
+class Scavenger(NPC):
+    def __init__(self, name):
+        self.name = name
+        self.desc = "An agile character that recycles everything he finds around to his benefit."
+        self.stats = {'str': 6, 'agi': 10, 'int': 4}
+        self.hpMult = 10
+        self.mpMult = 3
+        super().__init__(self.name, self.desc, self.stats, self.hpMult, self.mpMult)
+
+    def spScavenge(self, item: Item):
+        # TODO make new battle items? find battle items?
+        printc(f"{self.name} takes a quick look around and finds a {colors.CYAN}{item.name} under a pile of trash.")
---
+++ items.py
@@ -0,0 +1,4 @@
+class Item():
+    def __init__(self, name: str, iType: str):
+        self.name = name
+        self.iType = iType
